f67c52747a62090d614f92d5e00152254ea864db
Onecode24 <angelo24yanve@gmail.com>
2022-07-21 13:33:13 +0100

    javaScript methods

--- js_methods/src/App.vue
+++ js_methods/src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <h2>js_methods for JavaScript in vue</h2>
+  <h3>45+14+5 = {{addNumber(45,14,5)}}</h3>
+  <p v-for="name in names" :key="name">{{name}}</p>
+  <button @click="ramdonNumber">Clicked</button>
+  <p @dblclick="name += 'o'">double cliked me: {{name}}</p>
+  <h3>{{count}}</h3>
+  <button v-on:click="increment(1)">Increment 1</button><br>
+  <button v-on:click="increment(5)">Increment 5</button><br>
+  <button @click="decrement(1)">Décrement 1</button><br>
+  <button @click="decrement(5)">Decrement 5</button><br>
+
+  <p>{{title}}</p>
+  <button @click="changeName($event), increment(2,$event)">Change Name</button>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
 
+export default {
+  data(){
+    return {
+      names: ['Salon','Sillone','Salimatou','Romain','Danois','Zozoni','Demoniaon'],
+      name: 'Angelo',
+      count: 0,
+      title: 'SOUS TITRE'
+    }
+  },
+  methods: {
+    addNumber(a,b,c){
+      return a+b+c;
+    },
+    changeName(){
+      this.title= 'HIGH TITLE'
+    },
+    ramdonNumber(){
+      this.names = this.names.sort(()=>Math.random()-0.5);
+    },
+    increment(a){
+      this.count+=a;
+    },
+    decrement(a){
+      this.count-=a;
+    }
+  }
 }
 </script>
 
