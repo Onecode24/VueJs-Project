23af344a255f2d14fd00546ac064a06eebc2dd20
Onecode24 <angelo24yanve@gmail.com>
2022-07-21 13:38:27 +0100

    Vue Rendering

--- helloWorld/src/App.vue
+++ helloWorld/src/App.vue
@@ -1,16 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div> {{name}} </div>
+  <div v-text="source"></div>
+  <div v-html="html"></div>
+  <h2 v-bind:id="id">Header</h2>
+  <button v-bind:disabled="disabled">Button</button>
+  <h2 v-bind:style="{
+    color: highlight
+  }">Binding style in html</h2>
+  <h2 v-bind:style='style'>Style Object from data</h2>
+
+  <p v-if="num === 0">v-if for number is zero</p>
+  <p v-else-if="num < 0">v-else-if for number is negative</p>
+  <p v-else-if="num > 0">v-else-if for number is positif</p>
+  <p v-else >v-else for NOT A NUMBER</p>
+
+  <h3 v-show="showElement">Using v-show attribute</h3>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      name :"Vue3 projet",
+      source  : "Code Evolution in youtube",
+      html:"<b>Bold effet</b>",
+      id: 'headId',
+      disabled: false,
+      highlight: "orange",
+      style :{
+        color : "#2fe2ae",
+        fontWeight : "bold",
+        widht: 200,
+        border : "1px solid black"
+      },
+      num: 'a',
+      showElement: true
+    } 
   }
+
 }
 </script>
 
---
+++ vue_rendering/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <h3 v-for="(name,index) in names" :key="name" v-bind:class="textColor">{{index}} {{name}}</h3>
+  <p v-for="full in fullname" :key="full.first"> My name is {{full.first}} {{full.last}}</p>
+
+  <div v-for="actor in actors" :key="actor.name">
+    <h3 :class="nameStyle">{{actor.name}}</h3>
+    <p v-for="movie in actor.movies" :key="movie">{{movie}}</p>
+  </div> 
+
+  <h4 v-for="(value, key) in objectInfos" :key="value">{{key}} : {{value}}</h4>
+
+  <!--to see difference when we use :key attribute and not use-->
+
+  <template v-for="(name,index) in keyNames" :key="name">
+    <p v-if="index===0">This is the first element</p>
+    <h4>{{name}} at index {{index}}</h4>
+    <input type="text" placeholder="your name"/>
+    <hr>
+
+  </template>
+  <button @click="shuffle">Shuffle</button>
+
+
+</template>
+
+<script>
+
+export default{
+  data(){
+    return{
+      names : ['Angelo','Gabrielle','Solange'],
+      fullname: [
+        {first: 'SALONMON',last: 'Romain'},
+        {first: 'PAUL', last: 'Simon'},
+        {first: 'DEMONE', last: 'Emeraude'}
+      ],
+      actors:[
+        {
+          name:'Angelo',
+          movies: ['Underground 8','Fall in street war']
+        },
+        {
+          name:'Serge',
+          movies: ['Titanic 22','Back in prison'] 
+        }
+      ],
+      objectInfos : {
+        name : 'Solania',
+        age : 25,
+        sex: 'women'      
+      },
+
+      //names for :key attributes test
+      keyNames : ['Tony', 'Stark', 'Paul', 'Vincent','Tony'],
+
+      textColor : 'text-color',
+      nameStyle: 'actorName'
+    };
+  },
+  methods: {
+    shuffle(){
+      this.keyNames = this.keyNames.sort(()=> Math.random()-0.5);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.text-color{
+  color: #2ed402;
+}
+
+.actorName{
+  color: chocolate;
+  text-decoration: dashed;
+}
+</style>
