0fbfef7d855a6960ada7c53732e6111a9f163e32
Onecode24 <angelo24yanve@gmail.com>
2022-07-25 22:10:06 +0100

    getter & setters

--- computed_properties/src/App.vue
+++ computed_properties/src/App.vue
@@ -1,12 +1,27 @@
 <template>
   
-  <h2>My name is {{fullName}}</h2>
+  <h2>My name is {{fullName }}</h2>
   <p> La moyenne des notes donne {{moyenne}} </p>
-  <button @click="notes.push(10)">Add 10 to notes</button>
- 
+  <button @click="notes.push(10)">Add 10 to notes</button> <hr>
+  <h1>Use v-for an v-if on the products list</h1>
+
+  <template v-for="p in products" :key="p.id">
+    <h2 v-if="p.price >500">{{p.name}} {{p.price}}</h2>
+  </template>
+  <hr>
+  <h1>Use computed properties to filter a products table</h1>
+  <template v-for="p in productsFilter" :key="p.id">
+    <h2>{{p.name}} at {{p.price}}</h2>
+  </template>
+  <hr>
+
+  <h2>{{fullName}}</h2>
+  <button @click="changefullName">Change name</button>
+
 </template>
 
 <script>
+
 
 export default {
   data(){
@@ -14,12 +29,50 @@ export default {
       lastName: "Dolched",
       firstName: "Gabanane",
       notes : [12,14,5,2,10,15,4,7.5,14,18,19,15,20,0,3,5,5],
+      country : '',
+      products:[
+        {
+          id: 1,
+          name: 'Laptop',
+          price: 1400
+        },
+        {
+          id: 2,
+          name: 'Smartphone',
+          price: 500
+        },
+        {
+          id: 3,
+          name: 'Tablet',
+          price: 700
+        },
+        {
+          id: 4,
+          name: 'Desktop',
+          price: 1000
+        },
+      ]
     }
   },
-  methods:{},
+  methods:{
+    getfullName(){
+      this.full = this.lastName+' '+this.firstName
+      console.log(this.country);
+    },
+    changefullName(){
+      this.fullName = 'Demon King'
+    },
+  },
   computed: {
-    fullName(){
-      return `${this.lastName} ${this.firstName}`
+    fullName:{
+      get(){
+        return this.lastName+' '+this.firstName
+      },
+      set(value){
+        const names = value.split(' ')
+        this.lastName = names[0]
+        this.firstName = names[1]
+      }
     },
     moyenne(){
       var moy=0;
@@ -27,7 +80,11 @@ export default {
         moy+=this.notes[i]
       }
       return moy/this.notes.length;
+    },
+    productsFilter(){
+      return this.products.filter(p => p.price > 500)
     }
+    
   }
 
 
