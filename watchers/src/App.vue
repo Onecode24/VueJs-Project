a9923293f9783d78ae4b4a4d1ef8beb724862d46
Onecode24 <angelo24yanve@gmail.com>
2022-07-27 08:37:19 +0100

    watchers

---
+++ watchers/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>Music Volume(0-20)</h2>
+  <input type="range" v-model="volume" min="0" max="20" />
+  <p>{{volume}}</p>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      volume : 0
+    }
+  },
+  methods: {
+    view() {
+      console.log(this.volume);
+    }
+  },
+  watch: {
+    volume(newValue,oldValue) {
+      if(newValue == 15 && newValue>oldValue) {
+       alert('volume is high');
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
