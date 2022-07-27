e2120e23aff70d9d31b828cb3f47cf31aee35b76
Onecode24 <angelo24yanve@gmail.com>
2022-07-28 00:20:02 +0100

    immediate & deep attribut with handler method

--- watchers/src/App.vue
+++ watchers/src/App.vue
@@ -2,6 +2,23 @@
   <h2>Music Volume(0-20)</h2>
   <input type="range" v-model="volume" min="0" max="20" />
   <p>{{volume}}</p>
+  <div>
+    <label for="movie">Movie name</label>
+    <input type="text" name="movie" id="mobie" v-model="movie">
+  </div>
+  <div>
+    <label for="movie">Movie title</label>
+   <input type="text" name="title" id="title" v-model="movieInfos.title">
+  </div>
+  <div>
+    <label for="movie">Principal Actor</label>
+    <input type="text" name="actor" id="actor" v-model="movieInfos.actor">
+  </div>
+  <div>
+    <button @click="movieList=movieList.concat(['Wonder Woman'])">add movie</button>
+  </div>
+  
+  
 </template>
 
 <script>
@@ -9,7 +26,13 @@
 export default {
   data(){
     return{
-      volume : 0
+      volume : 0,
+      movie: 'Nil',
+      movieInfos:{
+        title:'',
+        actor:'',
+      },
+      movieList: ['Batman','Superman']
     }
   },
   methods: {
@@ -21,6 +44,23 @@ export default {
     volume(newValue,oldValue) {
       if(newValue == 15 && newValue>oldValue) {
        alert('volume is high');
+      }
+    },
+    movie:{
+      handler(newValue){
+        console.log("search movie named "+ newValue);
+      },
+      immediate: true
+    },
+    movieInfos:{
+      handler(newValue){
+        console.log("Search title: "+newValue.title+" with actor "+newValue.actor);
+      },
+      deep: true
+    },
+    movieList:{
+      handler(newValue){
+        console.log("Movie "+newValue+" is added to list");
       }
     }
   }
